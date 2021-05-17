d004a5d7d0d2968a180203d03990b959fc44412c
Diogo <diogoaluai@gmail.com>
2021-05-17 17:04:32 +0100

    Adds functionality to buttons.

--- src/graphics.c
+++ src/graphics.c
@@ -5,20 +5,15 @@
 #include <gtk/gtk.h>
 
 
+void onClickPrint(GtkApplication* app, gpointer user_data) {
+     printf("Heelo: %c\n", *(char *)user_data );
+}
+
 int main(int argc, char *argv[]) {
      GtkWidget *window;
-     GtkWidget *fixed;
-     GtkWidget *btn1;
-     GtkWidget *btn2;
-     GtkWidget *btn3;
      GtkWidget *vbox;
      GtkWidget *vbox2;
-     GtkWidget *one;
-     GtkWidget *two;
-     GtkWidget *three;
      GtkWidget *button;
-     gtk_init(&argc, &argv);
-
      gchar *values[12] = {
           "1", "2 abc", "3 def",
           "4 ghi", "5 jkl", "6 mno",
@@ -26,6 +21,10 @@ int main(int argc, char *argv[]) {
           "* +", "0 __", "#"
      };
 
+
+     gtk_init(&argc, &argv);
+
+     //--------------------------Window----------------------------------
      window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
      gtk_window_set_title(GTK_WINDOW(window), "Center");
      gtk_window_set_default_size(GTK_WINDOW(window), 250, 350);
@@ -37,6 +36,7 @@ int main(int argc, char *argv[]) {
 
 
 
+     //--------------------------Text View----------------------------------
 
      GtkWidget *view;
      GtkTextBuffer *buffer;
@@ -53,6 +53,7 @@ int main(int argc, char *argv[]) {
      gtk_box_pack_start(GTK_BOX(vbox), view, TRUE, TRUE, 0);
 
 
+     //--------------------------Middle buttons----------------------------------
 
      vbox2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 1);
      button = gtk_button_new_with_label("cycle");
@@ -63,6 +64,7 @@ int main(int argc, char *argv[]) {
      gtk_box_pack_start(GTK_BOX(vbox), vbox2, FALSE,FALSE, 8);
 
 
+     //--------------------------Grid----------------------------------
 
      GtkWidget *grid;
      grid = gtk_grid_new();
@@ -72,6 +74,7 @@ int main(int argc, char *argv[]) {
      for (int pos=0,i=0; i < 8; i+=2) {
           for (int j=0; j < 12; j+=4) {
                button = gtk_button_new_with_label(values[pos]);
+               g_signal_connect(button, "pressed", G_CALLBACK(onClickPrint), (void*)&(values[pos][0]));     //onClick event
                gtk_grid_attach(GTK_GRID(grid), button, j, i, 4, 2);
                pos++;
           }
@@ -80,13 +83,15 @@ int main(int argc, char *argv[]) {
      gtk_box_pack_start(GTK_BOX(vbox), grid, FALSE,FALSE, 20);
 
 
-
-
-
+     //--------------------------End----------------------------------
      gtk_widget_show_all(window);
      g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
      gtk_main();
+
+
+
+
+
 
      return 0;
 }
