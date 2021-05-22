c9550a476f6d1100250797f0a8ad6122c275dc5f
Diogo Barbosa <pessoal.dbarbosa@gmail.com>
2021-05-22 11:04:45 +0100

    fixed callbacks.h

--- src/callbacks.h
+++ src/callbacks.h
@@ -1,8 +1,8 @@
 #ifndef _CALLBACKS
 #define _CALLBACKS
 
-void on_button_clicked(GtkButton* button, gpointer param);
-void on_delete_clicked(GtkButton* button, gpointer param);
+void on_button_clicked(GtkButton* button, ButtonData* data);
+void on_delete_clicked(GtkButton* button, AppData* data);
 void on_t9_switch(GObject* sw, GParamSpec* pspec, AppData* data);
 
 #endif
